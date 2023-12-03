f155d3ae8d2c3ae4747274d5cf8c815f5e09c252
Samuel Pires <spires@ipn.pt>
2023-12-03 01:13:07 +0000

    Add svgs; Fix Css; Fix HomePage; Footer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,4 @@
-// Home.jsx
-
+// Home.js
 import React from 'react';
 import './Home.css';
 
@@ -7,19 +6,25 @@ function Home() {
   const backgroundImage = './images/home.jpg';
 
   return (
-    <div className="background-container">
-      <nav className="navbar">
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#contacts">Contacts</a></li>
-          <li><a href="#about">About</a></li>
-        </ul>
-      </nav>
-
-      <img src={backgroundImage} alt="Imagem de fundo" className="background-image" />
-      <div className="centered-text">
-        <h1>Gym</h1>
+    <div className="home">
+      <div className="dashboard">
+        <div>
+          <h1 className="titulo-grande">Keep your</h1>
+          <h1 className="titulo-grande">Body Active</h1>
+          <h3 className="subtitulo-pequeno">All tips on a fit body</h3>
+          <img className="circle" src={process.env.PUBLIC_URL + '/images/svg/circle.svg'} alt="Circle" />
+          <img className="line3" src={process.env.PUBLIC_URL + '/images/svg/line3.svg'} alt="Line3" />
+        </div>
       </div>
+      <div className="slide">
+        <p>sdsd</p>
+        <p>sdsd</p>
+        <p>sdsd</p>
+        <p>sdsd</p>
+      </div>
+      <footer className="footer">
+        <p>euu</p>
+      </footer>
     </div>
   );
 }
