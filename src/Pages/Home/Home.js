703ee24e8ef4fe19c52b674edd5785989dc5ed1c
Samuel Pires <spires@ipn.pt>
2023-12-05 02:00:22 +0000

    Add View All; Add 4 Exercises in Home; Fix CSS

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import ItemSlide from '../../Components/ItemSlide/ItemSlide';
 import Footer from '../../Components/Footer/Footer';
+import Exercise from '../../Components/Exercise/Exercise';
 import './Home.css';
 
 function Home() {
-  const backgroundImage = './images/home.jpg';
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async (url) => {
+      try {
+        setIsLoading(true);
+        let response = await fetch(url);
+        let dataJson = await response.json();
+        return dataJson;
+      } catch (error) {
+        console.error('Erro ao carregar dados:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    function getRandomItems(data) {
+      const randomIndices = [];
+      const randomItems = [];
+
+      while (randomIndices.length < 4) {
+        const randomIndex = Math.floor(Math.random() * data.length);
+
+        if (!randomIndices.includes(randomIndex)) {
+          randomIndices.push(randomIndex);
+          randomItems.push(data[randomIndex]);
+        }
+      }
+
+      return randomItems;
+    }
+
+    const fetchDataExercises = async () => {
+      setIsLoading(true);
+      let dataJson = await fetchData('./serverJson/all.json');
+
+      let dataJsonFour = getRandomItems(dataJson);
+
+      let dataUpperCase = dataJsonFour.map((item) => ({
+        ...item,
+        name: item.name.replace(/(?:^|\s)\S/g, char => char.toUpperCase())
+      }));
+
+      setData(dataUpperCase);
+
+      setIsLoading(false);
+    };
+
+    fetchDataExercises();
+  }, []);
 
   return (
     <div className="home">
@@ -21,11 +73,20 @@ function Home() {
         </div>
       </div>
       <div className="space"></div>
+      <Link to={"/exercises"}>
+        <div className="view-all">
+          <img className="hastag" src={process.env.PUBLIC_URL + '/images/svg/hastag.svg'} />
+          View All
+        </div>
+      </Link>
       <div className="slide">
-        <ItemSlide />
-        <ItemSlide />
-        <ItemSlide />
-        <ItemSlide />
+        {data.map((image, index) => (
+          <Exercise index={index} image={image} />
+        ))}
+
+        {isLoading ? (
+          Array.from({ length: 4 }, (_, index) => <ItemSlide key={index} />)
+        ) : null}
       </div>
       <Footer />
     </div>
